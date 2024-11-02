7488dc127a1da2e82717a09d9ac2070ba2642481
肥田 幸久(hz190162wz) <hz190162wz@jindai.jp>
2024-11-02 12:20:26 +0900

    a

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const FPS = 60;
 
 const CAN_W = 256;
 const CAN_H = 224;
-const PX_RATIO = 2;
+const PX_RATIO = 0.6;
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -16,6 +16,8 @@ canvas.style.height = CAN_H * PX_RATIO + "px";
 canvas.style.imageRendering = "pixelated";
 
 
+const CAM_SIZE_RATIO = 200;
+const CAM_F_LEN = 1;
 
 
 const G = 9.8;
@@ -39,17 +41,17 @@ class Bullet {
     }
 
     draw() {
-        const dw = 1 * this.width / this.y * 200;
+        const dw = CAM_F_LEN * this.width / this.y * CAM_SIZE_RATIO;
         const dx = CAN_W / 2 - dw / 2;
-        const dy = CAN_H / 2 + (1 * 1 / this.y) * 200;
-        const dy2 = CAN_H / 2 + (1 * 1 / (this.y - this.length)) * 200;
+        const dy = CAN_H / 2 + (-this.z * CAM_F_LEN / this.y) * CAM_SIZE_RATIO;
+        const dy2 = CAN_H / 2 + (-this.z * CAM_F_LEN / Math.max(this.y - this.length, 0)) * CAM_SIZE_RATIO;
 
         ctx.fillStyle = "white";
         ctx.fillRect(dx, dy, dw, dy2 - dy);
     }
 }
 
-const BULLET = new Bullet(0, 0, 0, 1, 0.05, 1);
+const BULLET = new Bullet(0, 0, -0.5, 100, 0.5, 5);
 
 
 function mainLoop() {
